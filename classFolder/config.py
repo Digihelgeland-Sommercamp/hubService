51b8841cdff9f2cb2f3783a8203f6777370b4946
Johannes-s-b <johannesborgaas@hotmail.com>
2021-07-19 14:28:42 +0200

    changed hostnames

--- classFolder/config.py
+++ classFolder/config.py
@@ -1,6 +1,6 @@
-hostname_fregservice="http://fregservice"
+hostname_fregservice="http://fregservice-svc"
 hostname_skattservice="http://skatteservice-svc"
-hostname_evaluatorservice="http://evaluatorservice"
+hostname_evaluatorservice="http://evaluatorservice-svc"
 hostname_exposedataservice="http://"
 port_fregservice=":5000"
 port_skattservice=":5000"
