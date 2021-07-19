313e3ffb5af997bf462b70e145842317499ecb25
Johannes-s-b <johannesborgaas@hotmail.com>
2021-07-19 12:51:56 +0200

    added print to debug

--- classFolder/contacter.py
+++ classFolder/contacter.py
@@ -11,7 +11,8 @@ class Contacter():
 
     def get_skattemelding(self, inntektsaar, personidentifikator):
         url = self.skatteserviceURL + "get_skattemelding/" + str(inntektsaar) + "/" + str(personidentifikator)
-        print(url)
+        print("ASJKDASDKJASD URL: "+url)
+        return
         r = requests.get(url)
         print(r.status_code)
         if r.status_code != 200:
