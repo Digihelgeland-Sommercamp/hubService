17c59a5e4f35e74ad4ec676e72b6f5d7a6a33002
Johannes-s-b <johannesborgaas@hotmail.com>
2021-07-19 15:42:44 +0200

    Added exposedataservice hostname

--- classFolder/config.py
+++ classFolder/config.py
@@ -1,7 +1,7 @@
 hostname_fregservice="http://fregservice-svc"
 hostname_skattservice="http://skatteservice-svc"
 hostname_evaluatorservice="http://evaluatorservice-svc"
-hostname_exposedataservice="http://"
+hostname_exposedataservice="http://exposeuserservice-svc"
 port_fregservice=":5000"
 port_skattservice=":5000"
 port_evaluatorservice=":5000"
